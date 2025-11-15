3e91da3ec3d38ce876293823613f76afd6d6298f
cqz20 <cqz20@cl-teaching-ecad.cl.cam.ac.uk>
2025-11-15 12:47:57 +0000

    Tick 1 code

--- updown/updowncounter.sv
+++ updown/updowncounter.sv
@@ -1,12 +1,18 @@
 module updowncounter
   (
-   input logic        clk,
-   input logic        rst,
-   input logic        up,
+   input logic            clk,
+   input logic            rst,
+   input logic            up,
    output logic [3:0] count
    );
 
    // insert your code here
-   assign count = 0;
-
+   always_ff @(posedge clk or posedge rst)
+	   if(rst)
+		   count <= 0;
+	   else
+		   if(up)
+			   count <= count == 4'b1111 ? 4'b0000 : count + 1;
+		   else
+			   count <= count == 4'b0000 ? 4'b1111 :  count - 1;
 endmodule
