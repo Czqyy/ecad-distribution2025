c0f8640ec6fab3ac9b50e51340a37b1ab6367e57
cqz20 <cqz20@cl-teaching-ecad.cl.cam.ac.uk>
2025-11-15 12:59:44 +0000

    Tick 2 code

--- twotrafficlights/twotrafficlights.sv
+++ twotrafficlights/twotrafficlights.sv
@@ -4,7 +4,34 @@ module twotrafficlights(
       output logic [2:0] lightsA, 
       output logic [2:0] lightsB
     );
-  logic [2:0] state;
-  // insert your code here
+  	logic [2:0] state, next_state;
+	logic b_active;
+  	// insert your code here
+	always_comb begin 
+		next_state[0] = state[2] & state[1];
+		next_state[1] = ~state[1];
+		next_state[2] = state[2] ^ state[1];
+	end
+  
+	always_ff @(posedge clk or posedge rst) begin
+		if(rst) begin
+			state <= 3'b110;
+			b_active <= 1'b1;
+			lightsA <= 3'b100;
+       			lightsB <= 3'b110;			
+		end else begin
+			state <= next_state;
 
+			if (next_state == 3'b100)
+				b_active <= ~b_active;
+
+			if (b_active) begin
+				lightsA <= 3'b100;
+				lightsB <= next_state;
+			end else begin
+				lightsA <= next_state;
+				lightsB <= 3'b100;
+			end
+		end
+	end
 endmodule
